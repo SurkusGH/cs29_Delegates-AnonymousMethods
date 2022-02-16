a4b1510e52d5f8337b690505f2fa147b3deb665f
Surkus <v.surkus@me.com>
2022-02-16 19:27:40 +0200

    Initial commit

---
+++ cs29_Delegates-AnonymousMethods/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace cs29_Delegates_AnonymousMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
